68bdfe9fe9936137e7dd23b315414485be883bab
Ahmed Ragab <ahmedasiawy@gmail.com>
2022-04-10 04:08:28 +0200

    Add elasticbeanstalk config file to git.

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,7 +1,6 @@
-cd ./udagram-api/www || exit 
 echo "Deploing udagram-api ..."
-eb init udagram-api -r us-east-1 -p node.js
+eb init udagram-api -r us-east-1 -p "Node.js 14 running on 64bit Amazon Linux 2"
 eb create --sample udagram-api-env
 eb use udagram-api-env
-eb setenv udagram-api-env POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_DB=udagram DB_PORT=5432 PORT=8080 POSTGRES_HOST=udagram.cwdvj1bzsca1.us-east-1.rds.amazonaws.com AWS_REGION=us-east-1f JWT_SECRET=ascvcjvdhbckcehbvhyfsscd URL=udagram.us-east-1.elasticbeanstalk.com
+eb setenv  POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_DB=udagram DB_PORT=5432 PORT=8080 POSTGRES_HOST=udagram.cwdvj1bzsca1.us-east-1.rds.amazonaws.com AWS_REGION=us-east-1f JWT_SECRET=ascvcjvdhbckcehbvhyfsscd URL=udagram.us-east-1.elasticbeanstalk.com
 eb deploy 
