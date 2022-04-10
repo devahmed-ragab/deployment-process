4ab467974882949a4b95a6f7bb4e0f09dfe81012
Ahmed Ragab <ahmedasiawy@gmail.com>
2022-04-10 05:01:34 +0200

    Add environment variables for S3. 	modified:   udagram-api/bin/deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -2,5 +2,7 @@ echo "Deploing udagram-api ..."
 eb init udagram-api -r us-east-1 -p "Node.js 14 running on 64bit Amazon Linux 2"
 eb create --sample udagram-api-env
 eb use udagram-api-env
-eb setenv  POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_DB=udagram DB_PORT=5432 PORT=8080 POSTGRES_HOST=udagram.cwdvj1bzsca1.us-east-1.rds.amazonaws.com AWS_REGION=us-east-1f JWT_SECRET=ascvcjvdhbckcehbvhyfsscd URL=udagram.us-east-1.elasticbeanstalk.com
+
+eb setenv POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_DB=udagram DB_PORT=5432 PORT=8080 POSTGRES_HOST=udagram.cwdvj1bzsca1.us-east-1.rds.amazonaws.com JWT_SECRET=ascvcjvdhbckcehbvhyfsscd AWS_REGION=us-east-1 AWS_BUCKET=udagram-frontend12736 URL=udagram-api-env.eba-drmbxa59.us-east-1.elasticbeanstalk.com
+
 eb deploy 
