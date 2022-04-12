fd05bc1e6c3be9ad106cb61ca349699a49e5db48
Ahmed Ragab <ahmedasiawy@gmail.com>
2022-04-12 04:46:20 +0200

    Add cricleci env variables to deploy scripts.     application_name     environment_name     AWS_DEFAULT_REGION 	modified:   udagram-api/bin/deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,6 +1,6 @@
 echo "Deploing udagram-api ..."
-eb init udagram-api -r us-east-1 -p "Node.js 14 running on 64bit Amazon Linux 2"
-eb create --sample udagram-api-env
-eb use udagram-api-env
+eb init $application_name -r $AWS_DEFAULT_REGION -p "Node.js 14 running on 64bit Amazon Linux 2"
+eb create --sample $environment_name
+eb use $environment_name
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB DB_PORT=$DB_PORT PORT=$PORT POSTGRES_HOST=$POSTGRES_HOST JWT_SECRET=$JWT_SECRET AWS_REGION=$AWS_REGION AWS_BUCKET=$AWS_BUCKET URL=$URL
 eb deploy 
